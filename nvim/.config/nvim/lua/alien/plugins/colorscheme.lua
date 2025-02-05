8211c20af925ed24c13a12fadaad0cd5282aee92
leomanangka <leomanangka@outlook.com>
2025-02-05 22:44:40 +0700

    remove colorscheme pywal

--- nvim/.config/nvim/lua/alien/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/alien/plugins/colorscheme.lua
@@ -76,12 +76,4 @@ return {
       transparent = true,
     },
   },
-  {
-  'uZer/pywal16.nvim',
-  -- for local dev replace with:
-  -- dir = '~/your/path/pywal16.nvim',
-  config = function()
-    vim.cmd.colorscheme("pywal16")
-  end,
-}
 }
